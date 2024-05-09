b54332b0a3707d4312465c20866738f49c82ae48
hmelica <hippolytemelica@icloud.com>
2024-05-09 15:10:41 +0200

    parse target written not tested yet

--- src/Message.cpp
+++ src/Message.cpp
@@ -18,11 +18,12 @@
 #include <string>
 
 Message::Message():
-    _method(),
-    _target(),
-    _header(),
-    _body  (),
-    _status(unset) {}
+    _method         (),
+    _target         (),
+    _header         (),
+    _response_header(),
+    _body           (),
+    _status         (unset) {}
 
 Message::~Message() {}
 
@@ -38,30 +39,61 @@ Message &Message::operator=(const Message &other) {
 
 HttpMethod Message::parse_method(const std::string &method, const size_t &end) {
     if (method.length() < 2 || end > MAX_METHOD)
-        throw HttpError(NotImplemented);
+        throw (HttpError(NotImplemented));
     if (method[0] == 'G' && method.find(method_string(GET)) == 0 && end == 3)
         return (GET);
     if (method[0] == 'P' && method.find(method_string(POST)) == 0 && end == 4)
         return (POST);
     if (method[0] == 'D' && method.find(method_string(DELETE)) == 0 && end == 6)
         return (DELETE);
-    throw HttpError(NotImplemented);
+    throw (HttpError(NotImplemented));
+}
+
+static void replace_all(std::string &str, const std::string &to_find, const std::string &to_replace) {
+    size_t elem = str.find(to_find, 0);
+
+    while (elem) {
+        str.replace(elem, to_find.length(), to_replace);
+        elem = str.find(to_find, 0);
+    }
+}
+
+void Message::parse_target(const std::string &in, const size_t &pos) {
+    size_t sp_protocol;
+    size_t protocol;
+
+    sp_protocol = in.find_first_of(" \t", pos + 1);
+    protocol    = in.find("HTTP", pos);
+    if (protocol == std::string::npos || sp_protocol == std::string::npos)
+        throw (HttpError(BadRequest));
+    if ((protocol - 1) - (pos + 1) > MAX_URI)
+        throw (HttpError(URITooLong));
+    if (sp_protocol != protocol - 1) { // there are SP remaining in URI, that is wrong, going for 301 MovedPermanently
+        std::string redirect = in.substr(pos + 1, (protocol - 1) - (pos + 1));
+        replace_all(redirect, " ",  "%20");
+        replace_all(redirect, "\t", "%09");
+        throw (HttpError(MovedPermanently, redirect));
+    }
+    _target = in.substr(pos + 1, (protocol - 1) - (pos + 1));
+    size_t host = _target.find("http://");
+    if (host == 0) {                   // absolute form
+        size_t host_end = _target.find("/", sizeof("http://"));
+        if (host_end == std::string::npos)
+            throw (BadRequest);
+        _header["Host"] = _target.substr(sizeof("http://"), host_end - sizeof("http://"));
+        _target         = _target.substr(sizeof("http://"), _target.length() - sizeof("http://"));
+    } else if (_target[0] != '/')      // not origin form
+        throw (BadRequest);
 }
 
 void Message::parse(const std::string &in) {
     size_t sp = in.find_first_of(" \t");
-    size_t protocol;
 
     try {
         _method = parse_method(in, sp);
     } catch (HttpError &e) {
         _method = none;
         _status = e.get_code();
-        return; // double check if anything needs to be done in case of error except returning
-    }
-    sp       = in.find_first_of(" \t", sp);
-    protocol = in.find("HTTP", sp);
-    if (sp != protocol - 1) {
-        ;
+        return;                        // double check if anything needs to be done in case of error except returning
     }
 }
--- src/Message.hpp
+++ src/Message.hpp
@@ -30,9 +30,12 @@ class Message {
         void                               parse(const std::string &in);
     private:
         HttpMethod                         parse_method(const std::string &method, const size_t &end);
+        void                               parse_target(const std::string &in, const size_t &pos);
         std::string                        _method;
         std::string                        _target;
         std::map<std::string, std::string> _header;
+        std::map<std::string, std::string> _response_header;
+        std::map<std::string, std::string> _parameters;
         std::string                        _body;
         HttpCode                           _status;
 
