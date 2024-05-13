a89e16a3cea97600920f391b302558127ab669b1
Nicolas Serve <nserve@student.42.fr>
2024-05-13 15:54:03 +0200

    - declare class ServerManager

---
+++ src/ServerManager.cpp
@@ -0,0 +1,3 @@
+#include "ServerManager.hpp"
+
+
---
+++ src/ServerManager.hpp
@@ -0,0 +1,28 @@
+#ifndef __INCLUDE_SRC_SERVERMANAGER_
+#define __INCLUDE_SRC_SERVERMANAGER_
+
+#include <string>
+#include <vector>
+class Server;
+
+class ServerManager {
+	
+private:
+	ServerManager(const std::string& configFile);
+	ServerManager(ServerManager &rhs);
+	void operator=(const ServerManager &rhs);
+
+	static ServerManager* instance;
+	std::string value;
+	std::vector<Server> Servers;
+
+public:
+	static ServerManager *getInstance(const std::string &configFile);
+	void deleteInstance();
+	~ServerManager();
+
+	Server &getServer(const std::string &serverName, int port);
+
+};
+
+#endif // !__INCLUDE_SRC_SERVERMANAGER_
