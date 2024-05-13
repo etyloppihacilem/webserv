0c6d621b7e8648f5a5a80a9b3b3757e25fab4429
Nicolas Serve <nserve@student.42.fr>
2024-05-13 17:52:43 +0200

    -designing the parsing lib for serverConf - start writing the definition

--- src/ServerConf.cpp
+++ src/ServerConf.cpp
@@ -1,3 +1,17 @@
 #include "ServerConf.hpp"
+#include <string>
+#include <algorithm>
 
+namespace ServerConf {
 
+	bool isValidHttp(const std::string &value)
+	{
+		if (*value.begin() == '{' && *value.rbegin() == '}'
+			&& std::find(value.begin(), value.end(), "server") != value.end())
+		{
+			return true;
+		}
+		return false;
+	}
+
+}
--- src/ServerConf.hpp
+++ src/ServerConf.hpp
@@ -1,53 +1,69 @@
 #ifdef INCLUDE_SRC_SERVERCONF_HPP_
 #define INCLUDE_SRC_SERVERCONF_HPP_
 
+#include <string>
+#include <vector>
+
 namespace ServerConf {
 
-static const int MAX_PORT = 65535;
+	static const int MAX_PORT = 65535;
+	typedef std::pair<std::string, std::string> Field;
 
-struct Fields {
+	struct Fields {
+		static std::vector<std::string> root(1);
+		static std::vector<std::string> base(1);
+		static std::vector<std::string> server(8);
+		static std::vector<std::string> location(8);
+	};
 
-	static std::vector<std::string> root(1);
-	static std::vector<std::string> base(1);
-	static std::vector<std::string> server(8);
-	static std::vector<std::string> location(8);
+	struct Module {
+		std::vector<Field> serverFields;
+		std::vector<Field> locationsFields;
+	};
 
-};
+	struct Config {
+		std::string root;
+		std::vector<std::string> serverList;
+		std::vector<Module> serverDetails;
+	};
 
-bool isValidHttp(const std::string &);
-bool isValidServer(const std::string &);
-bool isValidListen(const std::string &);
-bool isValidServerName(const std::string &);
-bool isValidRoot(const std::string &);
-bool isValidMethods(const std::string &);
-bool isValidClientMaxBodySize(const std::string &);
-bool isValidAutoindex(const std::string &);
-bool isValidIndex(const std::string &);
-bool isValidCgiPath(const std::string &);
-bool isValidUploadPath(const std::string &);
-bool isValidRewrite(const std::string &);
-bool isValidErrorPage(const std::string &);
-bool isValidFileExt(const std::string &);
+	std::vector<Field> tokenizeServer(const std::string &);
+	std::vector<Field> tokenizeLocation(const std::string &);
 
-};
+	bool isValidHttp(const std::string &);
+	bool isValidServer(const std::string &);
+	bool isValidListen(const std::string &);
+	bool isValidServerName(const std::string &);
+	bool isValidRoot(const std::string &);
+	bool isValidMethods(const std::string &);
+	bool isValidClientMaxBodySize(const std::string &);
+	bool isValidAutoindex(const std::string &);
+	bool isValidIndex(const std::string &);
+	bool isValidCgiPath(const std::string &);
+	bool isValidUploadPath(const std::string &);
+	bool isValidRewrite(const std::string &);
+	bool isValidErrorPage(const std::string &);
+	bool isValidFileExt(const std::string &);
 
-ServerConf::root[0] = "http";
-ServerConf::base[0] = "server";
-ServerConf::server[0] = "listen";
-ServerConf::server[1] = "root";
-ServerConf::server[2] = "index";
-ServerConf::server[3] = "server_name";
-ServerConf::server[4] = "client_max_body_size";
-ServerConf::server[5] = "methods";
-ServerConf::server[6] = "location";
-ServerConf::server[7] = "error_page";
-ServerConf::location[0] = "root";
-ServerConf::location[1] = "index";
-ServerConf::location[2] = "autoindex";
-ServerConf::location[3] = "methods";
-ServerConf::location[4] = "rewrite";
-ServerConf::location[5] = "upload_path";
-ServerConf::location[6] = "cgi_path";
-ServerConf::location[7] = "file_ext";
+}
+
+ServerConf::Fields::root[0] = "http";
+ServerConf::Fields::base[0] = "server";
+ServerConf::Fields::server[0] = "listen";
+ServerConf::Fields::server[1] = "root";
+ServerConf::Fields::server[2] = "index";
+ServerConf::Fields::server[3] = "server_name";
+ServerConf::Fields::server[4] = "client_max_body_size";
+ServerConf::Fields::server[5] = "methods";
+ServerConf::Fields::server[6] = "location";
+ServerConf::Fields::server[7] = "error_page";
+ServerConf::Fields::location[0] = "root";
+ServerConf::Fields::location[1] = "index";
+ServerConf::Fields::location[2] = "autoindex";
+ServerConf::Fields::location[3] = "methods";
+ServerConf::Fields::location[4] = "rewrite";
+ServerConf::Fields::location[5] = "upload_path";
+ServerConf::Fields::location[6] = "cgi_path";
+ServerConf::Fields::location[7] = "file_ext";
 
 #endif // __INCLUDE_SRC_SERVERCONF_
